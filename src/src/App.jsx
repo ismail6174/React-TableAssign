b262781df4f224cc0fd04df38e4761ac8db29267
Ismail Shah <ismailpx.6174@gmail.com>
2025-07-29 09:58:57 +0500

    Add files via upload

---
+++ src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserTable from './UserTable';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
